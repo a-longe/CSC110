64998756cdeb194d2f154dcbdd0df0cf80995323
Alex Longe <alex.m.longe@gmail.com>
2023-09-06 10:21:26 -0700

    day one

---
+++ 6-9-23/python_test.py
@@ -0,0 +1,26 @@
+# you can use "ctrl + /" to comment out many line of code at the same time
+# 4
+# 4 + 5
+
+# print(4, 5, "hello")
+
+# print(4 + 5 + 6)
+
+# single line comment
+
+"""
+multi
+line
+comment
+"""
+
+#print('it\'s raining today')
+
+total = 5 + 2 # total is a variable and contains the value returned by (5+2)
+print(1,total)
+
+total = 10
+print(2, total)
+
+total = total + 11
+print(3, total)
